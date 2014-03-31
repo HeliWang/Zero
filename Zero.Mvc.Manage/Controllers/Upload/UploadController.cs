74731129f6064e0c062874f8c2d190893818dfba
daitoudage <panzhongwei123>
2014-03-31 17:12:28 +0800

    mei

---
+++ Zero.Mvc.Manage/Controllers/Upload/UploadController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zero.Mvc.Manage.Controllers.Upload
+{
+    public class UploadController : BaseController
+    {
+        public ActionResult Photo()
+        {
+            return View();
+        }
+    }
+}
