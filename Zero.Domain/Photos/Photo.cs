4ac9dbf3e7bbc57605709b9da1f871fd0546fd48
daitoudage <723202874@qq.com>
2014-03-31 23:24:44 +0800

    tt

---
+++ Zero.Domain/Photos/Photo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zero.Domain.Photos
+{
+    public class Photo
+    {
+        public int Id { get { return PhotoId; } }
+
+        public int PhotoId { get; set; }
+
+        public int CateId { get; set; }
+
+        public int Url { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public DateTime UpdateTime { get; set; }
+    }
+}
