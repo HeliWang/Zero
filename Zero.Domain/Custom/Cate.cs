84c7edc0fa28732d482cf908fe6771cb8f99019e
daitoudage <723202874@qq.com>
2014-07-25 18:23:17 +0800

    qq

---
+++ Zero.Domain/Custom/Cate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zero.Domain.Custom
+{
+    public class Cate
+    {
+        public int CateId { get; set; }
+
+        public string CateName { get; set; }
+
+        public string TableName { get; set; }
+    }
+}
---
+++ Zero.Domain/Custom/Custom.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zero.Domain.Custom
+{
+    public class Custom
+    {
+        /// <summary>
+        /// 自定义编号
+        /// </summary>
+        public int CustomId { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 列表编号
+        /// </summary>
+        public string Ids { get; set; }
+
+        /// <summary>
+        /// 是否缓存
+        /// </summary>
+        public int IsCache { get; set; }
+
+        /// <summary>
+        /// 列表条件
+        /// </summary>
+        public string Condition { get; set; }
+
+        /// <summary>
+        /// 拼接后的sql语句
+        /// </summary>
+        public string Sql { get; set; }
+    }
+}
